8455df2682b80ffbaddcb83b22c1bd600710d86b
KIMHYOUNGMIN <rlagud2005@naver.com>
2023-01-05 23:12:19 +0900

    ⚙️

---
+++ src/Router.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./screens/Home";
+import Root from "./Root";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+import SignUp from "./screens/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <SignUp />,
+      },
+    ],
+    errorElement: <NotFound />,
+  },
+]);
+
+export default router;
